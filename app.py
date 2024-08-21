e26ec7f98ab48d884b8cc7cb3fdfda1cab76651f
poujoux <163121081+poujoux@users.noreply.github.com>
2024-08-21 12:28:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def greet():
     data = request.json
     name = data.get('name', 'Guest')
-    return jsonify({'message': f'Hello, {name}!'})
+    return jsonify({'message': f'Hello, {name}!'}), 200#httpstatuscode
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, ssl_context=('cert.pem', 'key.pem'))#forhttpscertificateissues
